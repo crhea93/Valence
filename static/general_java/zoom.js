1a7ac94d4a5bc8b97280dd988c8563daeea3e011
Carter Rhea <carterrhea93@gmail.com>
2022-04-05 14:31:35 -0400

    Fnished zoom updates

--- static/general_java/zoom.js
+++ static/general_java/zoom.js
@@ -4,14 +4,15 @@ $(document).ready(function(){
         $('#btn_ZoomIn').click(
             function () {
                 let cam_div = $('#CAM_items');
-                console.log(cam_div.css('min-height'))
                 if (currentZoom < 1.5) {
                     currentZoom += .1
-                    cam_div.css({'height': cam_div.height*currentZoom})
-                    cam_div.css('min-height', cam_div.css('min-height').replace(/[^-\d\.]/g, '')*currentZoom)
-                    cam_div.css({'height': cam_div.height*currentZoom})
-                    cam_div.animate({'zoom': currentZoom},
-                        {});
+                    //cam_div.css({'height': cam_height_init*currentZoom})
+                    //cam_div.css('min-height', cam_div.css('min-height').replace(/[^-\d\.]/g, '')*currentZoom)
+                    cam_div.animate({'zoom': currentZoom,
+                        'min-height': cam_height_init/currentZoom+'px',
+                        'max-height':cam_height_init/currentZoom+'px'},
+                        );
+
                     $('#zoom_lev').html('Zoom: '+currentZoom.toFixed(1));
                 }
             });
@@ -20,9 +21,13 @@ $(document).ready(function(){
                 let cam_div = $('#CAM_items');
                 if (currentZoom > 0.6) {
                     currentZoom -= .1
-                    cam_div.animate({'zoom': currentZoom},
+                    //cam_div.css({'height': cam_height_init*currentZoom})
+                    //cam_div.css('min-height', cam_div.css('min-height').replace(/[^-\d\.]/g, '')*currentZoom)
+                    cam_div.animate({'zoom': currentZoom,
+                        'min-height': cam_height_init/currentZoom+'px',
+                        'max-height': cam_height_init/currentZoom+'px'},
                         );
-                    cam_div.css('min-height', cam_height_init/currentZoom)
+
                     $('#zoom_lev').html('Zoom: '+currentZoom.toFixed(1));
                 }
             });
