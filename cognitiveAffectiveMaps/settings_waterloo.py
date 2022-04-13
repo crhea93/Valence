cdaccf34fd12e1eaa68dedd3e5df8df31b7dfeed
Carter Rhea <carterrhea93@gmail.com>
2022-04-12 22:04:17 -0400

    seettings

--- cognitiveAffectiveMaps/settings_waterloo.py
+++ cognitiveAffectiveMaps/settings_waterloo.py
@@ -128,9 +128,9 @@ AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
 AWS_LOCATION = 'static'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+#STATICFILES_DIRS = [
+#    os.path.join(BASE_DIR, 'static'),
+#]
 STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
