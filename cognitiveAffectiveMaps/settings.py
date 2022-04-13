af4b0c7bbfcdeb30f65912132395d63b03111968
Carter Rhea <carterrhea93@gmail.com>
2022-04-12 22:59:50 -0400

    seettings

--- cognitiveAffectiveMaps/settings.py
+++ cognitiveAffectiveMaps/settings.py
@@ -143,7 +143,7 @@ if os.getenv('WATERLOO') is not None:
     AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
-    AWS_DEFAULT_ACL = None
+    #AWS_DEFAULT_ACL = None
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
     AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
     # s3 static settings
