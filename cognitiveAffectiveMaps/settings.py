7063f4c46093102035b02d6f88e7d231bbfa6115
Carter Rhea <carterrhea93@gmail.com>
2025-10-24 21:29:38 -0400

    Move static path

--- cognitiveAffectiveMaps/settings.py
+++ cognitiveAffectiveMaps/settings.py
@@ -157,9 +157,9 @@ AWS_STORAGE_BUCKET_NAME = os.getenv("AWS_STORAGE_BUCKET_NAME")
 AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
 AWS_S3_OBJECT_PARAMETERS = {"CacheControl": "max-age=86400"}
 # s3 static settings
-STATIC_LOCATION = "static"
-STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{STATIC_LOCATION}/"
-STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+# STATIC_LOCATION = "static"
+# STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{STATIC_LOCATION}/"
+# STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 # s3 public media settings
 PUBLIC_MEDIA_LOCATION = "media"
 MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/"
@@ -168,9 +168,9 @@ django_heroku.settings(locals(), staticfiles=False)
 # else:
 # Static files (CSS, JavaScript, Images)
 #    pass
-# STATIC_URL = "/static/"
-# STATIC_ROOT = os.path.join(BASE_DIR, "")
-# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "")
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # MEDIA_URL = "media/"
 # django_heroku.settings(locals())
